80451fea02a9629e2deb06469c12af74e0719757
Adam Myhand <rhematicmovement@gmail.com>
2023-10-08 12:22:43 -0500

    zoo router

--- server/routes/zoo-router.js
+++ server/routes/zoo-router.js
@@ -1,10 +1,24 @@
-const express = require('express');
-const pool = require('../db/pool.js');
+const express = require("express");
+const pool = require("../db/pool.js");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // YOUR CODE HERE
+router.get("/", (req, res) => {
+  const queryText = `
+  SELECT "species".species_name, "class".class_name
+  FROM "species"
+  JOIN "class" ON "class".id = "species".class_id
+  ORDER BY "species".species_name ASC;
+  `;
+  pool
+    .query(queryText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log("Error making GET request", error);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
